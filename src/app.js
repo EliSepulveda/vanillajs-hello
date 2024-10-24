3e89b09aeac2db63b140855c734d6b9bb52fceaa
EliSepulveda <111314446+EliSepulveda@users.noreply.github.com>
2024-10-24 00:06:31 +0000

    generar todos los dominios posibles

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateDomains() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          console.log(pronoun + adj + noun + domain);
+        }
+      }
+    }
+  }
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandom();
 };
